e627f35da3d01381629a6deb4a70f1681b73832d
Mokuichi <38586357+Mokuichi147@users.noreply.github.com>
2020-03-22 04:04:05 +0900

    [add] 再生ボタンの画像

--- make-image.py
+++ make-image.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import math
 from PIL import Image, ImageDraw
 
 separator = '/'
@@ -11,10 +12,22 @@ resources_path = dir_path + separator + 'Resources'
 if not os.path.isdir(resources_path):
     os.mkdir(resources_path)
 
-size = 512
+size = 256
+main_color = (145,140,255, 255)
+sub_color  = ( 90, 90,255, 255)
+
+half = size / 2
+quarter = size / 4
+triangle_width = size / 4 * math.sqrt(3)
 
 img = Image.new('RGBA', (size,size), (0,0,0,0))
 draw = ImageDraw.Draw(img)
-draw.ellipse([150,150,size-150,size-150], fill=(145,140,255, 255))
-
+draw.ellipse(((size/3.5,size/3.5) ,(size/3.5*2.5,size/3.5*2.5)), fill=main_color)
 img.save(resources_path + separator + 'cursor.png')
+
+img = Image.new('RGBA', (size,size), (0,0,0,0))
+draw = ImageDraw.Draw(img)
+draw.polygon((((size-triangle_width)/2,quarter), ((size-triangle_width)/2,quarter*3), ((triangle_width)+quarter,half)), fill=main_color)
+img.save(resources_path + separator + 'playback_button.png')
+draw.polygon((((size-triangle_width)/2,quarter), ((size-triangle_width)/2,quarter*3), ((triangle_width)+quarter,half)), fill=sub_color)
+img.save(resources_path + separator + 'playback_button_down.png')
