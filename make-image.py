b88bb229b7aaa47af1e9f4c4b56bed63d30709e3
Mokuichi <38586357+Mokuichi147@users.noreply.github.com>
2020-03-22 16:11:48 +0900

    [add] 0フレーム目に移動するボタンの画像

--- make-image.py
+++ make-image.py
@@ -20,21 +20,65 @@ half = size / 2
 quarter = size / 4
 triangle_width = size / 4 * math.sqrt(3)
 
+
 img = Image.new('RGBA', (size,size), (0,0,0,0))
 draw = ImageDraw.Draw(img)
-draw.ellipse(((size/3.5,size/3.5) ,(size/3.5*2.5,size/3.5*2.5)), fill=main_color)
+draw.ellipse([
+    size/3.5, size/3.5,
+    size/3.5*2.5, size/3.5*2.5
+    ], fill=main_color)
 img.save(resources_path + separator + 'cursor.png')
 
-img = Image.new('RGBA', (size,size), (0,0,0,0))
-draw = ImageDraw.Draw(img)
-draw.polygon((((size-triangle_width)/2,quarter), ((size-triangle_width)/2,quarter*3), ((triangle_width)+quarter,half)), fill=main_color)
-img.save(resources_path + separator + 'playback_button.png')
-draw.polygon((((size-triangle_width)/2,quarter), ((size-triangle_width)/2,quarter*3), ((triangle_width)+quarter,half)), fill=sub_color)
-img.save(resources_path + separator + 'playback_button_down.png')
 
 img = Image.new('RGBA', (size,size), (0,0,0,0))
 draw = ImageDraw.Draw(img)
-draw.rectangle(((quarter,quarter), (quarter*3,quarter*3)), fill=main_color)
+draw.polygon([
+    (size-triangle_width)/2, quarter,
+    (size-triangle_width)/2, quarter*3,
+    triangle_width+quarter, half
+    ], fill=main_color)
+img.save(resources_path + separator + 'playback_button.png')
+draw.polygon([
+    (size-triangle_width)/2, quarter,
+    (size-triangle_width)/2, quarter*3,
+    triangle_width+quarter, half
+    ], fill=sub_color)
+img.save(resources_path + separator + 'playback_button_down.png')
+
+
+img = Image.new('RGBA', (size,size), (0,0,0,0))
+draw = ImageDraw.Draw(img)
+draw.rectangle([
+    quarter, quarter,
+    quarter*3, quarter*3
+    ], fill=main_color)
 img.save(resources_path + separator + 'playback_stop_button.png')
-draw.rectangle(((quarter,quarter), (quarter*3,quarter*3)), fill=sub_color)
+draw.rectangle([
+    quarter, quarter,
+    quarter*3, quarter*3
+    ], fill=sub_color)
 img.save(resources_path + separator + 'playback_stop_button_down.png')
+
+
+img = Image.new('RGBA', (size,size), (0,0,0,0))
+draw = ImageDraw.Draw(img)
+draw.polygon([
+    (size-triangle_width)/2, half,
+    triangle_width+quarter, quarter*3,
+    triangle_width+quarter, quarter
+    ], fill=main_color)
+draw.line([
+    (size-triangle_width)/2, quarter,
+    (size-triangle_width)/2, quarter*3
+    ], fill=main_color, width=size//10)
+img.save(resources_path + separator + 'set_zero_frame_button.png')
+draw.polygon([
+    (size-triangle_width)/2, half,
+    triangle_width+quarter, quarter*3,
+    triangle_width+quarter, quarter
+    ], fill=sub_color)
+draw.line([
+    (size-triangle_width)/2, quarter,
+    (size-triangle_width)/2, quarter*3
+    ], fill=sub_color, width=size//10)
+img.save(resources_path + separator + 'set_zero_frame_button_down.png')
