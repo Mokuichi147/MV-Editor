4aaaf8cdbf4e2347dd6f42c9b7a4c657a3b807ae
Mokuichi <38586357+Mokuichi147@users.noreply.github.com>
2020-04-09 03:06:56 +0900

    [update] 関数の整理

--- utils/core.py
+++ utils/core.py
@@ -98,7 +98,7 @@ class ProjectData:
             self.sounds += [i for i in _content if i['type'] == 'sound']
             self.videos += [i for i in _content if i['type'] == 'video']
     
-    def se_frame(self, content):
+    def __se_frame(self, content):
         _start_frame = content['start_frame']
         _end_frame = _start_frame + content['frame'][1] - content['frame'][0]
         return _start_frame, _end_frame
@@ -107,25 +107,24 @@ class ProjectData:
         _content_index = []
         for i, _content_list in enumerate(self.content):
             for j, _content in enumerate(_content_list):
-                _sf, _ef = self.se_frame(_content['start_frame'])
+                _sf, _ef = self.__se_frame(_content['start_frame'])
                 if _sf < frame_count < _ef:
                     _content_index.append([i, j])
         return _content_index
     
-    def check_content_block(self, content_index, frame=(0,0)):
-        for _content in self.content[content_index]:
-            _sf, _ef = self.se_frame(_content['start_frame'])
+    def __check_content_block(self, contents, frame=(0,0)):
+        for _content in contents:
+            _sf, _ef = self.__se_frame(_content['start_frame'])
             if not (_ef < frame[0] or frame[1] < _sf):
                 return False
         return True
     
     def content_index(self, data):
-        _frame = self.se_frame(data)
-        _content_count = len(self.content)
-        for i in range(_content_count):
-            if self.check_content_block(i, _frame):
+        _frame = self.__se_frame(data)
+        for i, contents in enumerate(self.content):
+            if self.__check_content_block(contents, _frame):
                 return i
-        return _content_count
+        return len(self.content)
     
     def add_image(self, path, animation_val=None, start_frame=0, frame=(0,0), size=None, pos=(0,0), angle=0):
         _data = {'type': 'image', 'path': path}
